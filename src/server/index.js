623eae4e70ba67ea8d20fa6a3cdf731fad545b46
Guy Farley <guy.e.farley@gmail.com>
2022-07-13 19:03:12 -0700

    Revised events for multiplayer functionality

--- src/server/index.js
+++ src/server/index.js
@@ -1,81 +1,81 @@
 'use strict';
 
 //---------------DB--------------------
-require('dotenv').config();
-const express = require('express');
-// const cors = require('cors');
-const app = express();
-// app.use(cors());
-app.use(express.json());
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-mongoose.connect(process.env.DB_URL);
-const PlayerData = require('./dbModel');
-// const authenticatePlayer = require('../auth/basicAuth');
-const DBPORT = process.env.DBPORT || 3004;
+// require('dotenv').config();
+// const express = require('express');
+// // const cors = require('cors');
+// const app = express();
+// // app.use(cors());
+// app.use(express.json());
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+// mongoose.connect(process.env.DB_URL);
+// const PlayerData = require('./dbModel');
+// // const authenticatePlayer = require('../auth/basicAuth');
+// const DBPORT = process.env.DBPORT || 3004;
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error'));
-db.once('open', function () {
-  console.log('Mongoose is connected');
-});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Connection error'));
+// db.once('open', function () {
+//   console.log('Mongoose is connected');
+// });
 
-//Routes
-app.get('/playerData', getPlayerData);
-app.post('/playerData', postPlayerData);
-// app.post('/basicAuth', authenticatePlayer);
+// //Routes
+// app.get('/playerData', getPlayerData);
+// app.post('/playerData', postPlayerData);
+// // app.post('/basicAuth', authenticatePlayer);
 
-async function getPlayerData(req, res, next) {
-  try {
-    let allPlayers = await PlayerData.find();
-    res.status(200).send(allPlayers);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send('server error');
-  }
-}
+// async function getPlayerData(req, res, next) {
+//   try {
+//     let allPlayers = await PlayerData.find();
+//     res.status(200).send(allPlayers);
+//   } catch (e) {
+//     console.error(e);
+//     res.status(500).send('server error');
+//   }
+// }
 
-async function postPlayerData(req, res, next) {
-  try {
-    // whenever username/password is provided, must READ all players in database, to see if username is new or existing
+// async function postPlayerData(req, res, next) {
+//   try {
+//     // whenever username/password is provided, must READ all players in database, to see if username is new or existing
 
-    let { Username, Password, Highscore } = req.body;
-    let player = {
-      Username: Username,
-      Password: Password,
-      Highscore: Highscore,
-    };
-    let playerData = await PlayerData.create(player);
-    console.log(player, playerData);
-    //allPlayers.forEach(async element => {
-    // if (element.Username === username) {
-    //   // if username is existing, compare the password, if password is bad, re-prompt for username and password
-    // } else { // if username is new, hash the password and POST the player
-    // let player = {
-    //   Username: username,
-    //   Password: password,
-    //   Highscore: null,
-    // };
-    // player.Password = await bcrypt.hash(player.Password, 10);
-    // let response = await PlayerData.create(player);
-    res.status(200).send(playerData);
-    //   }
-    // });
-    // console.log(allPlayers);
-  } catch (err) {
-    next(err);
-  }
-}
+//     let { Username, Password, Highscore } = req.body;
+//     let player = {
+//       Username: Username,
+//       Password: Password,
+//       Highscore: Highscore,
+//     };
+//     let playerData = await PlayerData.create(player);
+//     console.log(player, playerData);
+//     //allPlayers.forEach(async element => {
+//     // if (element.Username === username) {
+//     //   // if username is existing, compare the password, if password is bad, re-prompt for username and password
+//     // } else { // if username is new, hash the password and POST the player
+//     // let player = {
+//     //   Username: username,
+//     //   Password: password,
+//     //   Highscore: null,
+//     // };
+//     // player.Password = await bcrypt.hash(player.Password, 10);
+//     // let response = await PlayerData.create(player);
+//     res.status(200).send(playerData);
+//     //   }
+//     // });
+//     // console.log(allPlayers);
+//   } catch (err) {
+//     next(err);
+//   }
+// }
 
-app.get('*', (req, res) => {
-  res.status(404).send('Not available');
-});
+// app.get('*', (req, res) => {
+//   res.status(404).send('Not available');
+// });
 
-app.use((error, req, res, next) => {
-  res.status(500).send(error.message);
-});
+// app.use((error, req, res, next) => {
+//   res.status(500).send(error.message);
+// });
 
-app.listen(DBPORT, () => console.log(`Listening on PORT ${DBPORT}`));
+// app.listen(DBPORT, () => console.log(`Listening on PORT ${DBPORT}`));
 //---------------DB--------------------
 
 
@@ -116,31 +116,38 @@ socketSays.on('connection', (socket) => {
   });
 
   socket.on('AUTHENTICATED', (payload) => {
-    console.log('joined the room');
-    socket.join(payload.user.Username);
-    console.log('authenticated payload', payload);
-    socketSays.emit('MAIN', payload);
-  });
 
-  socket.on('PLAY_GAME', (payload) => {
-    socketSays.emit('START', payload);
-  });
-
-  socket.on('CORRECT', (payload) => {
-    console.log('server received correct');
-    socketSays.emit('NEXT_SEQUENCE', payload);
-  });
-
-  socket.on('INCORRECT', (payload) => {
-    socketSays.emit('LOST', payload);
-  });
-
-  socket.on('VIEW_HIGH_SCORES', (payload) => {
-    socketSays.emit('DISPLAY_HIGH_SCORES', payload);
+    // takes in payload, defines player-specific room, joins the socket to that room, emits MAIN with player-specific payload:
+    let clientRoom = payload.user.Username;
+    console.log(`${clientRoom} joined the ${clientRoom} room`);
+    socket.join(clientRoom);
+    console.log('Authenticated payload', payload);
+    socketSays.to(clientRoom).emit('MAIN', payload);
   });
 
   socket.on('RETURN_TO_MAIN', (payload) => {
-    socketSays.emit('MAIN', payload);
+    socketSays.to(payload.user.Username).emit('MAIN', payload);
   });
 
+  socket.on('PLAY_GAME', (payload) => {
+    // takes in player-specific payload
+    // emits START to that player's room, with player-specific payload
+    socketSays.to(payload.user.Username).emit('START', payload);
+  });
+
+  socket.on('VIEW_HIGH_SCORES', (payload) => {
+    socketSays.to(payload.user.Username).emit('DISPLAY_HIGH_SCORES', payload);
+  });
+
+  socket.on('CORRECT', (payload) => {
+    // takes in player-specific payload
+    // emits NEXT_SEQUENCE to that player's room, with player-specific payload
+    // console.log('server received correct');
+    socketSays.to(payload.user.Username).emit('NEXT_SEQUENCE', payload);
+  });
+  socket.on('INCORRECT', (payload) => {
+    // takes in player-specific payload
+    // emits NEXT_SEQUENCE to that player's room, with player-specific payload
+    socketSays.to(payload.user.Username).emit('LOST', payload);
+  });
 });
