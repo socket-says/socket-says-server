c1a4e6e4d1ffe350b991cf81903592b4776f6c40
Brady Davenport <brady.davenport@pm.me>
2022-07-13 21:21:27 -0600

    refactored

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,14 @@
 'use strict';
 
 //---------------DB--------------------
+
 require('dotenv').config();
 const express = require('express');
-// const cors = require('cors');
 const app = express();
-// app.use(cors());
 app.use(express.json());
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 mongoose.connect(process.env.DB_URL);
 const PlayerData = require('./dbModel');
-// const authenticatePlayer = require('../auth/basicAuth');
 const DBPORT = process.env.DBPORT || 3004;
 
 const db = mongoose.connection;
@@ -20,52 +17,9 @@ db.once('open', function () {
   console.log('Mongoose is connected');
 });
 
-//Routes
-app.get('/playerData', getPlayerData);
-app.post('/playerData', postPlayerData);
-// app.post('/basicAuth', authenticatePlayer);
-
-async function getPlayerData(req, res, next) {
-  try {
-    let allPlayers = await PlayerData.find();
-    res.status(200).send(allPlayers);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send('server error');
-  }
-}
-
-async function postPlayerData(req, res, next) {
-  try {
-    // whenever username/password is provided, must READ all players in database, to see if username is new or existing
-
-    let { Username, Password, Highscore } = req.body;
-    let player = {
-      Username: Username,
-      Password: Password,
-      Highscore: Highscore,
-    };
-    let playerData = await PlayerData.create(player);
-    console.log(player, playerData);
-    //allPlayers.forEach(async element => {
-    // if (element.Username === username) {
-    //   // if username is existing, compare the password, if password is bad, re-prompt for username and password
-    // } else { // if username is new, hash the password and POST the player
-    // let player = {
-    //   Username: username,
-    //   Password: password,
-    //   Highscore: null,
-    // };
-    // player.Password = await bcrypt.hash(player.Password, 10);
-    // let response = await PlayerData.create(player);
-    res.status(200).send(playerData);
-    //   }
-    // });
-    // console.log(allPlayers);
-  } catch (err) {
-    next(err);
-  }
-}
+app.get('/', (req, res) => {
+  console.log('Welcome to the \'Socket-says\' server');
+});
 
 app.get('*', (req, res) => {
   res.status(404).send('Not available');
@@ -76,19 +30,19 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(DBPORT, () => console.log(`Listening on PORT ${DBPORT}`));
+
 //---------------DB--------------------
 
+//-------------Socket Server ---------------
 
 const { Server } = require('socket.io');
 const PORT = process.env.PORT || 3002;
-const chalk = require('chalk');
 const server = new Server(PORT);
 
 const socketSays = server.of('/socket-says');
 
 socketSays.on('connection', (socket) => {
   console.log('Socket connected to Event Server', socket.id);
-  let currentPlayer = 'guest';
   socketSays.emit('LOG_IN');
 
   socket.on('CHECK_USERNAME', async (payload) => {
@@ -98,7 +52,6 @@ socketSays.on('connection', (socket) => {
       let player = await PlayerData.findOne({ Username });
       console.log('player: ', player);
       if (player !== null) {
-        currentPlayer = player;
         socketSays.emit('PLAYER_EXISTS', payload);
       } else if (player === null) {
         socketSays.emit('NEW_PLAYER', payload);
