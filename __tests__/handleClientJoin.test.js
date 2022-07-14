b66250ed3d14d72dcc2080d42c76b0dcff4ba626
Brady Davenport <brady.davenport@pm.me>
2022-07-13 21:21:51 -0600

    test.todo

---
+++ __tests__/handleClientJoin.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const handleCLientJoin = require('../../src/server/handleClientJoin');
+
+describe('Tests for client joining the server', () => {
+  test.todo('Client joining the server, causes message to display on server screen', () => {
+    // client joining the server
+
+    // expect(console.log).toEqual(''); // causes message to display on server screen
+  });
+});
